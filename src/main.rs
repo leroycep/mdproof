5752ee483a34858b92a93b0f987447c30e35fcbb
LeRoyce Pearson <leroycepearson@geemili.xyz>
2018-09-06 19:33:33 -0500

    Add support for bold text

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,47 @@ extern crate pulldown_cmark as cmark;
 extern crate printpdf;
 
 use printpdf::*;
+use cmark::*;
 use std::fs::File;
 use std::io::BufWriter;
 
-const A4_DIMENSIONS: (f64, f64) = (210.0, 297.0);
+const A4_DIMENSIONS: (Mm, Mm) = (Mm(210.0), Mm(297.0));
 const DEFAULT_FONT: BuiltinFont = BuiltinFont::TimesRoman;
+const BOLD_FONT: BuiltinFont = BuiltinFont::TimesBold;
+const ITALIC_FONT: BuiltinFont = BuiltinFont::TimesItalic;
+const ITALIC_BOLD_FONT: BuiltinFont = BuiltinFont::TimesBoldItalic;
+const MARGIN: (Mm, Mm) = (Mm(20.0), Mm(20.0));
 
 fn main() {
-    let (mut doc, page1, layer1) = PdfDocument::new("Rust MD PDF", Mm(A4_DIMENSIONS.0), Mm(A4_DIMENSIONS.1), "Layer 1");
+    let (mut doc, page1, layer1) = PdfDocument::new("Rust MD PDF", A4_DIMENSIONS.0, A4_DIMENSIONS.1, "Layer 1");
     let current_layer = doc.get_page(page1).get_layer(layer1);
 
-    let text = "Lorem **ipsum**";
+    let markdown = "Lorem **ipsum**";
 
-    let font = doc.add_builtin_font(DEFAULT_FONT).unwrap();
-    current_layer.use_text(text, 48, Mm(10.0), Mm(200.0), &font);
+    let parser = Parser::new(markdown);
+
+    let normal_font = doc.add_builtin_font(DEFAULT_FONT).unwrap();
+    let bold_font = doc.add_builtin_font(BOLD_FONT).unwrap();
+    let mut font = &normal_font;
+
+    current_layer.begin_text_section();
+    current_layer.set_font(&font, 16);
+    current_layer.set_text_cursor(MARGIN.0, A4_DIMENSIONS.1-MARGIN.1);
+
+    let mut bold = false;
+    for event in parser {
+        match event {
+            Event::Start(Tag::Strong) => {
+                font = &bold_font;
+                current_layer.set_font(font, 16);
+            }
+            Event::End(Tag::Strong) => {
+                font = &normal_font;
+                current_layer.set_font(font, 16);
+            }
+            Event::Text(text) => current_layer.write_text(text, &font),
+            _ => {}
+        }
+    }
     doc.save(&mut BufWriter::new(File::create("test.pdf").unwrap())).unwrap();
 }
