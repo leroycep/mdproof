78f6da94db1505b22731e124240f3f04a5d78f3b
LeRoyce Pearson <leroycepearson@geemili.xyz>
2018-09-08 16:59:09 -0500

    Produce single page PDF a little bit of presentation

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 extern crate pulldown_cmark as cmark;
-extern crate printpdf;
+extern crate pdf_canvas;
 
-use printpdf::*;
+use pdf_canvas::{Pdf, BuiltinFont, FontSource};
 use cmark::*;
 use std::fs::File;
-use std::io::{BufWriter, Read};
+use std::io::Read;
 
-const A4_DIMENSIONS: (Mm, Mm) = (Mm(210.0), Mm(297.0));
-const DEFAULT_FONT: BuiltinFont = BuiltinFont::TimesRoman;
-const BOLD_FONT: BuiltinFont = BuiltinFont::TimesBold;
-const ITALIC_FONT: BuiltinFont = BuiltinFont::TimesItalic;
-const ITALIC_BOLD_FONT: BuiltinFont = BuiltinFont::TimesBoldItalic;
-const MARGIN: (Mm, Mm) = (Mm(20.0), Mm(20.0));
+/// PAGE_SIZE is the size of a sheet of A4 paper in pt
+const PAGE_SIZE: (f32, f32) = (595.0, 842.0);
+const MARGIN: (f32, f32) = (20.0, 20.0);
+const DEFAULT_FONT: BuiltinFont = BuiltinFont::Times_Roman;
+const DEFAULT_FONT_SIZE: f32 = 12.0;
+const BOLD_FONT: BuiltinFont = BuiltinFont::Times_Bold;
+const ITALIC_FONT: BuiltinFont = BuiltinFont::Times_Italic;
+
+const DEFAULT_OUTPUT_FILENAME: &str = "test.pdf";
 
 fn main() {
-    let (mut doc, page1, layer1) = PdfDocument::new("Rust MD PDF", A4_DIMENSIONS.0, A4_DIMENSIONS.1, "Layer 1");
-    let current_layer = doc.get_page(page1).get_layer(layer1);
+    let mut doc = Pdf::create(DEFAULT_OUTPUT_FILENAME).unwrap();
 
     let mut markdown_file = File::open("test.md").unwrap();
     let mut markdown = String::new();
@@ -23,28 +25,34 @@ fn main() {
 
     let parser = Parser::new(&markdown);
 
-    let normal_font = doc.add_builtin_font(DEFAULT_FONT).unwrap();
-    let bold_font = doc.add_builtin_font(BOLD_FONT).unwrap();
-    let mut font = &normal_font;
+    doc.render_page(PAGE_SIZE.0, PAGE_SIZE.1, |canvas| {
+        let font = canvas.get_font(DEFAULT_FONT);
+        let bold = canvas.get_font(BOLD_FONT);
+        let italic = canvas.get_font(ITALIC_FONT);
+        canvas.text(|t| {
+            t.set_font(&font, DEFAULT_FONT_SIZE)?;
+            t.set_leading(18.0)?;
+            t.pos(MARGIN.0, PAGE_SIZE.1-MARGIN.1)?;
 
-    current_layer.begin_text_section();
-    current_layer.set_font(&font, 16);
-    current_layer.set_text_cursor(MARGIN.0, A4_DIMENSIONS.1-MARGIN.1);
+            for event in parser {
+                match event {
+                    Event::Start(Tag::Strong) => t.set_font(&bold, DEFAULT_FONT_SIZE)?,
+                    Event::End(Tag::Strong) => t.set_font(&font, DEFAULT_FONT_SIZE)?,
+                    Event::Start(Tag::Emphasis) => t.set_font(&italic, DEFAULT_FONT_SIZE)?,
+                    Event::End(Tag::Emphasis) => t.set_font(&font, DEFAULT_FONT_SIZE)?,
 
-    let mut bold = false;
-    for event in parser {
-        match event {
-            Event::Start(Tag::Strong) => {
-                font = &bold_font;
-                current_layer.set_font(font, 16);
+                    Event::Text(text) => t.show(&text)?,
+
+                    Event::Start(Tag::Paragraph) => t.show_line(" ")?,
+                    Event::End(Tag::Paragraph) => t.show_line(" ")?,
+                    Event::HardBreak =>  t.show_line(" ")?,
+                    Event::SoftBreak => t.show_line("")?,
+                    _ => {}
+                }
             }
-            Event::End(Tag::Strong) => {
-                font = &normal_font;
-                current_layer.set_font(font, 16);
-            }
-            Event::Text(text) => current_layer.write_text(text, &font),
-            _ => {}
-        }
-    }
-    doc.save(&mut BufWriter::new(File::create("test.pdf").unwrap())).unwrap();
+            Ok(())
+        })
+    }).unwrap();
+
+    doc.finish().unwrap();
 }
