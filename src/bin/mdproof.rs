87aca6c851d8974af6e6fe54fd4279861b476aa5
LeRoyce Pearson <leroycepearson@geemili.xyz>
2018-09-12 17:08:50 -0500

    Make output file argument optional
    If the output file path is not supplied, the program changes the extension of
    the input file to "pdf".

--- src/bin/mdproof.rs
+++ src/bin/mdproof.rs
@@ -3,6 +3,7 @@ extern crate quicli;
 extern crate mdproof;
 
 use quicli::prelude::*;
+use std::path::Path;
 
 #[derive(Debug, StructOpt)]
 struct Cli {
@@ -10,13 +11,23 @@ struct Cli {
     markdown_file: String,
     /// Where to save the generated `.pdf` to
     #[structopt(long = "out", short = "o")]
-    output_file: String,
+    output_file: Option<String>,
 
     #[structopt(flatten)]
     verbosity: Verbosity,
 }
 
 main!(|args: Cli, log_level: verbosity| {
+    let output_path = match args.output_file {
+        Some(text) => Path::new(&text).to_path_buf(),
+        None => Path::new(&args.markdown_file).with_extension("pdf"),
+    };
     let cfg = mdproof::Config::default();
-    mdproof::run(&args.output_file, &args.markdown_file, &cfg)?;
+    mdproof::run(
+        output_path
+            .to_str()
+            .ok_or(format_err!("Could not convert path to string"))?,
+        &args.markdown_file,
+        &cfg,
+    )?;
 });
